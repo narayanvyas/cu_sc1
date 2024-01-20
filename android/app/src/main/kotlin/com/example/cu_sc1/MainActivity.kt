c0da35d9f10d97a8a204e97ef86fd5a8e0ff8951
Narayan Vyas <narayanvyas@Narayans-MacBook-Pro.local>
2024-01-20 16:16:12 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/cu_sc1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cu_sc1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'My nth App',
+    home: FirstPage(),
+  ));
+}
+
+// To create Stateful Page - Type "stf"
+// To create Stateless Page - Type "stl"
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        title: Text(
+          'My Page Name',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(30),
+        children: [
+          Text('Hello World'),
+          Container(
+            height: 30,
+          ),
+          Text('Hello World'),
+          TextField(),
+          ElevatedButton(
+              onPressed: () {
+                print('Button Clicked');
+              },
+              child: Text('Click Me')),
+          Image.network(
+              'https://cdn.pixabay.com/photo/2024/01/07/14/12/man-8493244_1280.jpg'),
+          Text('Hello World'),
+        ],
+      ),
+    );
+  }
+}
